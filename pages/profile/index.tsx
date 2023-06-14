0d80ca47350ea8aec2d2117a585f6052b225e81d
milottmorina <95414103+milottmorina@users.noreply.github.com>
2023-06-14 15:08:32 +0200

    Finished the Account Settings Page
    Brought the Change Password Functionality to Account Settings Page

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -11,9 +11,7 @@ interface User {
 
 const UserProfile: React.FC = () => {
   const [profile, setProfile] = useState<User | null>(null);
-  const [currentPassword, setCurrentPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmNewPassword, setConfirmNewPassword] = useState('');
+  
   const user = useUser();
 
   useEffect(() => {
@@ -34,32 +32,7 @@ const UserProfile: React.FC = () => {
     fetchProfile();
   }, [user.user]);
 
-  const handleChangePassword = async (e: React.FormEvent) => {
-    e.preventDefault();
-
-    if (newPassword !== confirmNewPassword) {
-      console.error('New password and confirm new password must match.');
-      return;
-    }
-
-    try {
-      const response = await axios.post(`http://localhost:8081/user/changepassword/${user.user?.id}`, {
-        currentPassword,
-        newPassword,
-        confirmNewPassword,
-      });
-      console.log('Password changed successfully:', response.data);
-      // Optionally, you can show a success message or perform additional actions
-    } catch (error) {
-      console.error('Error changing password:', error);
-      // Handle error and display appropriate message to the user
-    }
-
-    // Clear the form inputs
-    setCurrentPassword('');
-    setNewPassword('');
-    setConfirmNewPassword('');
-  };
+  
 
   if (!profile) {
     return <div>Loading...</div>;
@@ -72,22 +45,7 @@ const UserProfile: React.FC = () => {
       <p className={styles.info}>First Name: {profile.firstName}</p>
       <p className={styles.info}>Last Name: {profile.lastName}</p>
 
-      <form onSubmit={handleChangePassword} className={styles.form}>
-        <h2 className={styles.formTitle}>Change Password</h2>
-        <label>
-          Current Password:
-          <input type="password" value={currentPassword} onChange={(e) => setCurrentPassword(e.target.value)} />
-        </label>
-        <label>
-          New Password:
-          <input type="password" value={newPassword} onChange={(e) => setNewPassword(e.target.value)} />
-        </label>
-        <label>
-          Confirm New Password:
-          <input type="password" value={confirmNewPassword} onChange={(e) => setConfirmNewPassword(e.target.value)} />
-        </label>
-        <button type="submit">Change Password</button>
-      </form>
+     
     </div>
   );
 };
